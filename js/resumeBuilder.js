0e032ca8185354812a6b33f681c9795f69075ce8
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-08-25 21:23:59 -0400

    Edits

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 var bio = {
     "name": "Deborah E. Padilla",
     "role": "Front-End Web Developer",
-    "contact": {
+    "contacts": {
         "mobile": "201-749-6963",
         "email": "deborahelizabethpadilla@gmail.com",
         "github": "deborahepadilla",
@@ -9,7 +9,7 @@ var bio = {
         "location": "New York, NY"
     },
     "biopic": "images/bio.jpg",
-    "welcomeMsg": "I am a self-taught Web Developer.",
+    "welcomeMessage": "I am a self-taught Web Developer.",
     "skills": ["HTML", "CSS", "Javascript", "jQuery", "Bootstrap", "Brackets", "Google Chrome"]
 };
 
